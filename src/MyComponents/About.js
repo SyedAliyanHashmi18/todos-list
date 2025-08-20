21d6056a5c1357594edc535916e2c5a6e95813df
SyedAliyanHashmi18 <hashmisyedaliyan@gmail.com>
2025-08-21 01:41:13 +0500

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      <div>
+        This is an about component
+        <p>lorem30
+            adfafas
+        </p>
+      </div>
+    </div>
+  )
+}
+
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+export const Footer = () => {
+   
+  return (
+    <footer className="bg-dark text-light py-3" >
+        <p className="text-center bg-dark">Copyright &copy; </p>
+    </footer>
+  )
+}
+
+
